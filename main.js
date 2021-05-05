ad9ee8918d506f9a6b6bdc8c682c127c7df4ba6f
riverbook708 <riverbook708@gmail.com>
2021-05-05 18:45:54 +0900

    boilerplate code

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-const { win, BrowserWindow, app } = require('electron');
+const { BrowserWindow, app, ipcMain, Notification } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 600,
+    height: 400,
     backgroundColor: "white",
     webPreferences: {
       nodeIntegration: false,
       worldSafeExecuteJavaScript: true,
-      contextIsolation: true
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
   win.loadFile('index.html');
 }
+
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
+
+ipcMain.on('notify', (_, message) => {
+  new Notification({title: 'Notification', body: message}).show();
+})
 
 app.whenReady().then(createWindow);
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import './index.scss';
 
-ReactDOM.render(<h1>Hello React App</h1>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
