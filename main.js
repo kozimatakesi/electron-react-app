dd822159f5a2ef87a26b71032b0d5bdc35eb3afb
riverbook708 <riverbook708@gmail.com>
2021-05-07 19:38:03 +0900

    add menu bar first

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { BrowserWindow, app, ipcMain, Notification, dialog } = require('electron');
+const { BrowserWindow, app, ipcMain, Notification, dialog, Menu } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
@@ -33,6 +33,130 @@ const createWindow = () => {
       win.webContents.send('allFiles',files);
     })
   });
+  //menu追記
+
+
+    //Menuバーの作成
+  const isMac = process.platform === 'darwin'
+
+  const template = [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
+    // { role: 'fileMenu' }
+    {
+      label: 'ファイル',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' },
+        { label: 'open' ,
+          accelerator: 'Command+0',
+          click: async() => {
+            const filename = await dialog.showOpenDialog({
+              properties: ['openDirectory'],
+              title: 'ファイルを選択しよう'
+            });
+            const filepath = filename.filePaths[0];
+            console.log(filepath);
+            win.webContents.send('filename',filepath);
+            fs.readdir(filepath, (err, files) => {
+              console.log(files);
+              files.shift();
+              win.webContents.send('allFiles',files);
+            })
+          }
+        }
+      ]
+    },
+    // { role: 'editMenu' }
+    {
+      label: '編集',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac ? [
+          { role: 'pasteAndMatchStyle' },
+          { role: 'delete' },
+          { role: 'selectAll' },
+          { type: 'separator' },
+          {
+            label: 'Speech',
+            submenu: [
+              { role: 'startSpeaking' },
+              { role: 'stopSpeaking' }
+            ]
+          }
+        ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+        ])
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: '見かけ',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac ? [
+          { type: 'separator' },
+          { role: 'front' },
+          { type: 'separator' },
+          { role: 'window' }
+        ] : [
+          { role: 'close' }
+        ])
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://electronjs.org')
+          }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
+
 }
 
 if (isDev) {
--- src/js/File.js
+++ src/js/File.js
@@ -3,7 +3,10 @@ import React, { useEffect } from 'react';
 const File = () => {
 
   useEffect(() => {
+    //getElementByIdができないため、useEffectにてレンダリング後に実行
     const element = document.getElementById('list');
+
+    //channel:allFilesにsendがあったとき、指定ファイルパス内のファイルをリストで表示
     api.on('allFiles', (event, arg) => {
       element.innerHTML = '';
       arg.forEach(file => {
@@ -12,6 +15,7 @@ const File = () => {
     });
   },[]);
 
+  //channel:filenameにsendがあったとき、題名をファイルパスに書き換え
   api.on('filename', (event, arg) => document.querySelector('#hoge').textContent = arg);
 
 
